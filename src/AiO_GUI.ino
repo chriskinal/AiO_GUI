8e01f527a5f99a5d413270cd19046ec1802449ab
chriskinal <118364224+chriskinal@users.noreply.github.com>
2024-12-14 20:54:32 -0600

    Revert "Reapply "standardize names. Periodic format pretty.""
    This reverts commit 398e49966e58848b0857fe1baab78ae3e195a96b.

---
+++ src/AiO_GUI.ino
@@ -0,0 +1,39 @@
+#include "Arduino.h"
+#include "common.h"
+#include "gnss_handlers.h"
+#include "setup.h"
+#include "mongoose_start.h"
+#include "KeyaCANBUS.h"
+#include "Autosteer.h"
+#include "AutosteerPID.h"
+
+
+void setup() {
+  delay(3000); //Delay for tesing to allow opening serial terminal to see output
+  Serial.begin(115200);
+  while (!Serial) delay(50);
+  Serial.print("\r\n\n\n*********************\r\nStarting setup...\r\n");
+  Serial.print("Firmware version: ");
+  Serial.print(inoVersion);
+
+  LEDs.set(LED_ID::PWR_ETH, PWR_ETH_STATE::PWR_ON);
+
+  setCpuFrequency(600 * 1000000);           // Set CPU speed, default is 600mhz, 150mhz still seems fast enough, setup.ino
+  ethernet_init();
+  mongoose_init();
+  ipaddrSetup();
+  udpSetup();
+  serialSetup();                            // setup.h
+  parserSetup();                            // setup.h
+  BNO.begin(SerialIMU);                     // BNO_RVC.cpp
+  autosteerSetup();                         // Autosteer.h
+  CAN_Setup();                              //Start CAN3 for Keya
+
+  Serial.println("\r\n\nEnd of setup, waiting for GPS...\r\n"); 
+  delay(1);
+  resetStartingTimersBuffers();             // setup.ino
+}
+
+void loop() {
+  mongoose_poll();
+}
