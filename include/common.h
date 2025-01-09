c4968527199509d1da4d34fef7df3adbacd4bc4c
chriskinal <118364224+chriskinal@users.noreply.github.com>
2025-01-09 15:55:06 -0600

    Added SterrSettings to config json.

--- include/common.h
+++ include/common.h
@@ -4,7 +4,7 @@
 #include "Arduino.h"
 #include "HardwareSerial.h"
 #include "elapsedMillis.h"
-#include "config_utils.h"
+
 #include "EEPROM.h"
 #include <stdint.h>
 #include <Streaming.h>
@@ -115,6 +115,8 @@ struct SteerSettingsStruct
 };
 SteerSettingsStruct defaultSteerSettings;                        // 11 bytes
 struct SteerSettingsStruct steerSettings = defaultSteerSettings; // don't need 'struct' in front?
+
+#include "config_utils.h"
 
 uint8_t steerReading, prevSteerReading = 1; // currentState = 0
 int16_t pulseCount = 0;                     // Steering Wheel Encoder
