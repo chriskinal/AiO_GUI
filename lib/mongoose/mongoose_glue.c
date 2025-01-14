3da564602cebfadf7cd22488c2bcb794bfbf512a
chriskinal <118364224+chriskinal@users.noreply.github.com>
2025-01-14 15:36:00 -0600

    Revert "Clean up some debugging code."
    This reverts commit ef99e77e678034470dfb6ddba45bb41876f105a0.

--- lib/mongoose/mongoose_glue.c
+++ lib/mongoose/mongoose_glue.c
@@ -23,7 +23,9 @@ bool glue_check_save(void) {
 }
 void glue_start_save(void) {
   s_action_timeout_save = mg_now() + 1000; // Start save, finish after 1 second
+  //char *msg = mg_mprintf("%s,%d,%d,%d,%d,%o\r\n", s_settings.fversion, s_settings.bd_ip1, s_settings.bd_ip2, s_settings.bd_ip3, s_settings.gps_type, s_settings.gps_pass);
   MG_DEBUG(("s_setting: %s,%d,%d,%d,%d,%d", s_settings.fversion, s_settings.bd_ip1, s_settings.bd_ip2, s_settings.bd_ip3, s_settings.gps_type, s_settings.gps_pass));
+  //glue_get_settings(&s_config);
   saveConfig(filename, s_config);
 }
 
