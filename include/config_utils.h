c702712e66a3da36f3d553283eece56f52e9e2b3
chriskinal <118364224+chriskinal@users.noreply.github.com>
2025-01-14 15:42:40 -0600

    Revert "Interin pretty up code."
    This reverts commit a0c7947f474f9bb77e5b651ab973d6d25188e3b4.

--- include/config_utils.h
+++ include/config_utils.h
@@ -8,7 +8,7 @@
 // File system variables
 LittleFS_Program aioFS;
 #define NF 10
-static File files[NF + 1];
+static File files[NF+1];
 static int fsInit = 0;
 // End
 
@@ -17,62 +17,54 @@ struct Config
 {
   char fversion[25];
 };
-const char *filename = "/config.txt";
-// Config config;
+const char* filename = "/config.txt";
+//Config config;
 
 settings s_config;
 // End
 
 // Initialize LittleFS disk
-void fileInit()
-{
-  aioFS.begin(1 * 1024 * 1024);
-  Serial.print("\r\nLittleFS: initialized");
-  Serial.printf("\r\nTotal Size: %llu bytes, Used: %llu\r\n", aioFS.totalSize(), aioFS.usedSize());
-  for (int i = 0; i <= NF; i++)
-  {
-    files[i] = 0;
+void fileInit() {
+    aioFS.begin(1 * 1024 * 1024);
+    Serial.print("\r\nLittleFS: initialized");
+    Serial.printf("\r\nTotal Size: %llu bytes, Used: %llu\r\n", aioFS.totalSize(), aioFS.usedSize());
+    for (int i = 0; i <= NF; i++) { files[i] = 0; }
+    fsInit = 1;
   }
-  fsInit = 1;
-}
 
 // Load configuration - Should load default config if run for the first time
-void loadConfig(const char *filename, settings &s_config)
-{
-  Serial.println(F("Loading configuration..."));
-  // Serial.printf("%s,%d,%d,%d,%d,%o\r\n", s_config.fversion,s_config.bd_ip1,s_config.bd_ip2, s_config.bd_ip3, s_config.gps_type,s_config.gps_pass);
-  File file = aioFS.open(filename);
-  JsonDocument doc;
+void loadConfig(const char* filename, settings& s_config) {
+    Serial.println(F("Loading configuration..."));
+    //Serial.printf("%s,%d,%d,%d,%d,%o\r\n", s_config.fversion,s_config.bd_ip1,s_config.bd_ip2, s_config.bd_ip3, s_config.gps_type,s_config.gps_pass);
+    File file = aioFS.open(filename);
+    JsonDocument doc;
 
-  DeserializationError error = deserializeJson(doc, file);
-  if (error)
-    Serial.println(F("Failed to read config file, using default configuration"));
-  // Populate the configuation array from the JSON doc | from the default values
-  strlcpy(s_config.fversion,                     // <- destination
-          doc["fversion"] | "AiO v5.0a Web GUI", // <- source Note: value after the | is the default if "fversion" is empty in the JSON document.
-          sizeof(s_config.fversion));            // <- destination's capacity
-  s_config.bd_ip1 = doc["bd_ip1"] | 192;
-  s_config.bd_ip2 = doc["bd_ip2"] | 168;
-  s_config.bd_ip3 = doc["bd_ip3"] | 5;
-  s_config.gps_type = doc["gps_type"] | 2;
-  s_config.gps_pass = doc["gps_pass"] | false;
-  // End
-  glue_set_settings(&s_config);
-  file.close();
-}
+    DeserializationError error = deserializeJson(doc, file);
+    if (error) Serial.println(F("Failed to read config file, using default configuration"));
+        // Populate the configuation array from the JSON doc | from the default values
+        strlcpy(s_config.fversion,                  // <- destination
+                doc["fversion"] | "AiO v5.0a Web GUI",  // <- source Note: value after the | is the default if "fversion" is empty in the JSON document.
+                sizeof(s_config.fversion));         // <- destination's capacity
+        s_config.bd_ip1 = doc["bd_ip1"] | 192;
+        s_config.bd_ip2 = doc["bd_ip2"] | 168;
+        s_config.bd_ip3 = doc["bd_ip3"] | 5;
+        s_config.gps_type = doc["gps_type"] | 2;
+        s_config.gps_pass = doc["gps_pass"] | false;      
+        // End
+        glue_set_settings(&s_config);
+        file.close();
+  }
 
 // Saves the configuration to a file
-extern "C" void saveConfig(const char *filename, const settings &s_config)
-{
+extern "C" void saveConfig(const char* filename, const settings& s_config) {
   Serial.println(F("Saving configuration..."));
-  // glue_update_state();
+  //glue_update_state();
   glue_get_settings(&s_config);
   // Delete existing file, otherwise the configuration is appended to the file
   aioFS.remove(filename);
 
   File file = aioFS.open(filename, FILE_WRITE);
-  if (!file)
-  {
+  if (!file) {
     Serial.println(F("Failed to create file"));
     return;
   }
@@ -85,6 +77,7 @@ extern "C" void saveConfig(const char *filename, const settings &s_config)
   doc["bd_ip3"] = s_config.bd_ip3;
   doc["gps_type"] = s_config.gps_type;
   doc["gps_pass"] = s_config.gps_pass;
+<<<<<<< HEAD
   doc["ss_Kp"] = steerSettings.Kp;
   doc["ss_lowPWM"] = steerSettings.lowPWM;
   doc["ss_wasOffset"] = steerSettings.wasOffset;
@@ -110,13 +103,17 @@ extern "C" void saveConfig(const char *filename, const settings &s_config)
 
   if (serializeJson(doc, file) == 0)
   {
+=======
+  
+  if (serializeJson(doc, file) == 0) {
+>>>>>>> parent of a0c7947 (Interin pretty up code.)
     Serial.println(F("Failed to write to file"));
   }
 
   file.close();
-  Serial.printf("s_config: %s,%d,%d,%d,%d,%o\r\n", s_config.fversion, s_config.bd_ip1, s_config.bd_ip2, s_config.bd_ip3, s_config.gps_type, s_config.gps_pass);
+  Serial.printf("s_config: %s,%d,%d,%d,%d,%o\r\n", s_config.fversion,s_config.bd_ip1,s_config.bd_ip2, s_config.bd_ip3, s_config.gps_type,s_config.gps_pass);
   Serial.println(F("Saved configuration..."));
-}
+  }
 
 // Load configuration - Should load default config if run for the first time
 void loadConfig(const char *filename, settings &s_config)
@@ -165,24 +162,21 @@ void loadConfig(const char *filename, settings &s_config)
 }
 
 // Prints the JSON content of a file to the Serial port
-void printFile(const char *filename)
-{
+void printFile(const char* filename) {
   // Dump config file
   Serial.println(F("Print config file..."));
   File file = aioFS.open(filename);
-  if (!file)
-  {
+  if (!file) {
     Serial.println(F("Failed to read file"));
     return;
   }
 
-  while (file.available())
-  {
+  while (file.available()) {
     Serial.print((char)file.read());
   }
   Serial.println();
 
   file.close();
-}
+  }
 
 #endif
---
+++ src/AiO_GUI.ino
@@ -0,0 +1,79 @@
+#include "Arduino.h"
+#include "common.h"
+#include "udpHandlers.h"
+#include "gnss_handlers.h"
+#include "setup.h"
+#include "mongoose_start.h"
+#include "KeyaCANBUS.h"
+#include "Autosteer.h"
+#include "AutosteerPID.h"
+#include "serialRW.h"
+#include "gps1Poll.h"
+#include "gps2Poll.h"
+#include "gpsProc.h"
+
+void setup() {
+  delay(3000); //Delay for tesing to allow opening serial terminal to see output
+  Serial.begin(115200);
+  while (!Serial) delay(50);
+  Serial.print("\r\n\n\n*********************\r\nStarting setup...\r\n");
+  
+  LEDs.set(LED_ID::PWR_ETH, PWR_ETH_STATE::PWR_ON);
+
+  setCpuFrequency(600 * 1000000);           // Set CPU speed, default is 600mhz, 150mhz still seems fast enough, setup.ino
+  fileInit();
+  loadConfig(filename, s_config);
+  printFile(filename);
+  //glue_get_settings(&s_config);
+  //saveConfig(filename, s_config);
+  //aioFS.remove(filename);
+  Serial.print("Firmware version: ");
+  Serial.println(s_config.fversion);
+  Eth_EEPROM();
+  ethernet_init();
+  mongoose_init();
+  ipaddrSetup();
+  udpSetup();
+  serialSetup();                            // setup.h
+  parserSetup();                            // setup.h
+  BNO.begin(SerialIMU);                     // BNO_RVC.cpp
+  autosteerSetup();                         // Autosteer.h
+  CAN_Setup();                              //Start CAN3 for Keya
+  machinePTR = new MACHINE;
+  const uint8_t pcaOutputPinNumbers[8] = { 1, 0, 12, 15, 9, 8, 6, 7 };    // all 8 PCA9555 section/machine output pin numbers on v5.0a
+  const uint8_t pcaInputPinNumbers[]  = { 14, 13, 11, 10, 2, 3, 4, 5 };   // all 8 PCA9555 section/machine output "sensing" pin numbers on v5.0a
+  if ( outputs.begin() )
+  {
+    Serial.print("\r\nSection outputs (PCA9555) detected (8 channels, low side switching)");
+    machinePTR->init(&outputs, pcaOutputPinNumbers, pcaInputPinNumbers, 100); // mach.h
+  }
+
+  Serial.println("\r\n\nEnd of setup, waiting for GPS...\r\n"); 
+  delay(1);
+  resetStartingTimersBuffers();             // setup.ino
+  
+  
+}
+
+void loop() {
+  gps1Poll();
+  gps2Poll();
+  gpsProc();
+  KeyaBus_Receive();
+  autoSteerUpdate();
+  serialRW();
+  mongoose_poll();
+  LEDs.updateLoop();
+  machinePTR->watchdogCheck();
+
+  if (BNO.read()) {                         // there should be new data every 10ms (100hz)
+    bnoStats.incHzCount();
+    bnoStats.update(1);                     // 1 dummy value
+  }
+
+  // wait 40 msec (F9P) from prev GGA update, then update imu data for next PANDA sentence
+  if (imuPandaSyncTrigger && imuPandaSyncTimer >= 40) {
+    prepImuPandaData();
+    imuPandaSyncTrigger = false;       // wait for next GGA update before resetting imuDelayTimer again
+  }
+}
