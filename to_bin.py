48bcf7a58340c9fcf2942f619816b7fd8f31c96f
chriskinal <118364224+chriskinal@users.noreply.github.com>
2025-02-11 15:58:39 -0600

    C hnaged to_bin. py back since not cross platform.

--- to_bin.py
+++ to_bin.py
@@ -6,5 +6,3 @@ env.AddPostAction(
         "$BUILD_DIR/${PROGNAME}.elf", "$BUILD_DIR/${PROGNAME}.bin"
     ]), "Building $BUILD_DIR/${PROGNAME}.bin")
 )
-env.Execute("cp %s %s" % ("$BUILD_DIR/${PROGNAME}.hex", "$PROJECT_DIR/compiled_firmware/"))
-env.Execute("cp %s %s" % ("$BUILD_DIR/${PROGNAME}.bin", "$PROJECT_DIR/compiled_firmware/"))
