2e269b39a68f28bd0f87f79d41ac2c90c301bbcf
m-elias <eliaschief@gmail.com>
2025-02-12 12:13:07 -0600

    LED debug prints

--- lib/LEDS/LEDS.h
+++ lib/LEDS/LEDS.h
@@ -119,6 +119,15 @@ public:
 
   void init() {
     pinMode(GGA_LED, OUTPUT);
+
+    Serial.print("\r\nInitializing RGB LED Outputs");
+    Wire.beginTransmission(0x70);
+    Serial.print("\r\n- RGB PCA9685 ");
+    if (Wire.endTransmission() == 0)
+      Serial.print("found");
+    else
+      Serial.print("*NOT found!*");
+
     ledDriver.begin();
     Wire.setClock(1000000);
     ledDriver.setPWMFreq(120); // 120 hz should be enough to not notice, but increase if flickering
